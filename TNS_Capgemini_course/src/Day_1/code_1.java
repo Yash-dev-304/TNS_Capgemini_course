8ea7fa5131b712ae454d9411c4ce930b33a97c53
Yash Bhavsar <97103962+Yash-tommy@users.noreply.github.com>
2023-02-01 13:46:27 +0530

    Add files via upload

---
+++ TNS_Capgemini_course/src/Day_1/code_1.java
@@ -0,0 +1,7 @@
+package Day_1;
+
+public class code_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ TNS_Capgemini_course/src/Day_1/code_2.java
@@ -0,0 +1,8 @@
+package Day_1;
+
+public class code_2 {
+    public static void main(String[] args) {
+        int a= 'a';
+        System.out.println(a);
+    }
+}
