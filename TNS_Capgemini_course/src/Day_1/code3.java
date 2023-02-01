d1e70ce21a3c681f589fa0c13f1c04e664f077d3
Yash Bhavsar <97103962+Yash-tommy@users.noreply.github.com>
2023-02-01 14:00:58 +0530

    Add files via upload

---
+++ TNS_Capgemini_course/src/Day_1/code3.java
@@ -0,0 +1,16 @@
+package Day_1;
+
+public class code3 {
+    public static void main(String[] args) {
+        int value1 =9/2;
+		float value2 =101/61;
+		double value3 =10d/6d;
+		float value4 =5/2.5f;
+
+		System.out.println("value 1 is :" + value1);
+		System.out.println("value 2 is :" + value2);
+		System.out.println("value 3 is :" + value3);
+		System.out.println("value 4 is :" + value4);
+
+    }
+}
