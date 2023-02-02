fd3b14b9a62f9d0b5bf828d3a4101a31bcc3981e
Yash-dev-304 <bhavsaryash@kbtcoe.org>
2023-02-02 19:31:57 +0530

    .

---
+++ src/Day_2/OperatorDemo.java
@@ -0,0 +1,11 @@
+package Day_2;
+
+public class OperatorDemo {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int x=10;
+        x=(x==10)?1:0;
+        System.out.println(x);
+    }
+}
