a81d925fafbe5ea6112b0787ce55dbc1712cc1a7
Yash Bhavsar <97103962+Yash-dev-304@users.noreply.github.com>
2023-02-07 13:59:14 +0530

    Add files via upload

---
+++ TNS_Capgemini_course/src/Day_3/r1.java
@@ -0,0 +1,18 @@
+package Day_3;
+
+public class r1 {
+    int r=45;
+    String k= "almost";
+    String h="done";
+    public void j1(String o,int q)
+    {
+        System.out.println("Can You");
+    }
+
+    public static void main(String[] args) {
+        r1 g=new r1();
+        g.j1("Blue",76);
+        System.out.println(g.k);
+        System.out.println(g.k+" "+g.h);
+    }
+}
